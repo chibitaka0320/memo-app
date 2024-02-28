d9a9f72631eae22278cabf8ff18b3f42ebc1028d
takahiro <taka.araat32@gmail.com>
2024-02-28 19:47:26 +0900

    Make components

--- MemoApp/src/app/index.tsx
+++ MemoApp/src/app/index.tsx
@@ -1,50 +1,22 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Header } from '../components/Header'
+import { MemoListItem } from '../components/MemoListItem'
+import { CircleButton } from '../components/CircleButton'
 
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-        <View>
-            <View>
-                <Text>Memo App</Text>
-                <Text>ログアウト</Text>
-            </View>
-        </View>
+
+        <Header/>
 
         <View>
-            <View>
-                <View>
-                    <Text>買い物リスト</Text>
-                    <Text>2023年10月1日 10:00</Text>
-                </View>
-                <View>
-                    <Text>X</Text>
-                </View>
-            </View>
-
-            <View>
-                <View>
-                    <Text>買い物リスト</Text>
-                    <Text>2023年10月1日 10:00</Text>
-                </View>
-                <View>
-                    <Text>X</Text>
-                </View>
-            </View>
-
-            <View>
-                <View>
-                    <Text>買い物リスト</Text>
-                    <Text>2023年10月1日 10:00</Text>
-                </View>
-                <View>
-                    <Text>X</Text>
-                </View>
-            </View>
+            <MemoListItem/>
+            <MemoListItem/>
+            <MemoListItem/>
         </View>
 
-        <View>
-            <Text>+</Text>
-        </View>
+        <CircleButton>+</CircleButton>
+
     </View>
   )
 }
@@ -52,8 +24,7 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#ffffff'
   }
 })
 
