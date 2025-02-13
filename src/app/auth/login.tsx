e43def6ed2b7ba01b13106c6c16f20b0b5eaaa3c
chibitaka0320 <taka.araat32@gmail.com>
2025-02-13 15:17:00 +0900

    firebaseの初期設定

--- src/app/auth/login.tsx
+++ src/app/auth/login.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-native";
 import Button from "../../components/Button";
 import { Link, router } from "expo-router";
+import { useState } from "react";
 
 const handlePress = (): void => {
   // ログイン
@@ -14,12 +15,35 @@ const handlePress = (): void => {
 };
 
 const Login = (): JSX.Element => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Login</Text>
-        <TextInput style={styles.input} value="Email address" />
-        <TextInput style={styles.input} value="Password" />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="Email Address"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+          autoCapitalize="none"
+          secureTextEntry
+          placeholder="Password Address"
+          textContentType="password"
+        />
         <Button label="Submit" onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Not registerd?</Text>
--- src/app/auth/signup.tsx
+++ src/app/auth/signup.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-native";
 import Button from "../../components/Button";
 import { Link, router } from "expo-router";
+import { useState } from "react";
 
 const handlePress = (): void => {
   // サインアップ
@@ -14,12 +15,35 @@ const handlePress = (): void => {
 };
 
 const Signup = (): JSX.Element => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Signup</Text>
-        <TextInput style={styles.input} value="Email address" />
-        <TextInput style={styles.input} value="Password" />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="Email Address"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+          autoCapitalize="none"
+          secureTextEntry
+          placeholder="Password Address"
+          textContentType="password"
+        />
         <Button label="Submit" onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registerd?</Text>
