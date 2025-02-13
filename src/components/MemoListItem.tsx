fd537afa8b7929675e3412453de2d93edd9aac38
chibitaka0320 <taka.araat32@gmail.com>
2025-02-13 11:05:15 +0900

    TouchableOpacityの適用

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 const MemoListItem = (): JSX.Element => {
@@ -8,11 +8,10 @@ const MemoListItem = (): JSX.Element => {
         <Text style={styles.memoListItemTitle}>買い物リスト</Text>
         <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
       </View>
-      <View>
-        <Text>
-          <Entypo name="cross" size={24} />
-        </Text>
-      </View>
+
+      <TouchableOpacity>
+        <Entypo name="cross" size={32} color="#B0B0B0" />
+      </TouchableOpacity>
     </View>
   );
 };
